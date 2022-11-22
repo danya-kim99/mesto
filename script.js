ce051bcb485236b61fc4ca9e925ef2a83b42b303
Daniil Kim <danya-kim@yandex-team.ru>
2022-11-22 13:40:18 +0300

    adaptive

--- script.js
+++ script.js
@@ -2,15 +2,31 @@ let edit_button = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let close_button = document.querySelector('.popup__close-button');
 
-function add_class(element) {
-  element.classList.add('popup_opened');
+function open_popup() {
+  popup.classList.add('popup_opened');
+  document.querySelector('.popup__input:first-of-type').value = document.querySelector('.profile__name').textContent;
+  document.querySelector('.popup__input:nth-of-type(2)').value = document.querySelector('.profile__profession').textContent;
 }
 
-function remove_class(element) {
-  element.classList.remove('popup_opened');
+function close_popup() {
+  popup.classList.remove('popup_opened');
 }
 
 
-edit_button.addEventListener('click', add_class(popup));
 
-close_button.addEventListener('click', remove_class(popup));
+edit_button.addEventListener('click', open_popup);
+close_button.addEventListener('click', close_popup);
+
+let submit_button = document.querySelector('.popup__submit');
+let formElement = document.querySelector('.popup__container');
+
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  document.querySelector('.profile__name').textContent = document.querySelector('.popup__input:first-of-type').value;
+  document.querySelector('.profile__profession').textContent = document.querySelector('.popup__input:nth-of-type(2)').value;
+  popup.classList.remove('popup_opened');
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
