e0f20dc142b3c924e14ca8870b59525dc2dc69a8
Daniil Kim <danya-kim@yandex-team.ru>
2023-02-02 23:40:05 +0300

    addCard + like listeners

--- script.js
+++ script.js
@@ -25,19 +25,19 @@ function closePopupProfile() {
 editButton.addEventListener('click', openPopupProfile);
 closeButtonProfile.addEventListener('click', closePopupProfile);
 
-let submitButton = document.querySelector('.popup__submit');
-let formElement = document.querySelector('.popup__container');
+let profileFormElement = document.querySelector('.popup__container_profile');
 
-function formSubmitHandler(evt) {
+function profileFormSubmitHandler(evt) {
   evt.preventDefault();
   document.querySelector('.profile__name').textContent = document.querySelector('.popup__input_type_name').value;
   document.querySelector('.profile__profession').textContent = document.querySelector('.popup__input_type_profession').value;
   popupProfile.classList.remove('popup_opened');
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+profileFormElement.addEventListener('submit', profileFormSubmitHandler);
 
 //функционал поп-апа с добавлением места
+
 let addButton = document.querySelector('.profile__add-button')
 let popupPlace = document.querySelector('.popup_type_place');
 let closeButtonPlace = document.querySelector('.popup__close-button_place');
@@ -55,7 +55,23 @@ function closePopupPlace() {
 addButton.addEventListener('click', openPopupPlace);
 closeButtonPlace.addEventListener('click', closePopupPlace);
 
+//фунционал добавления карточки с местом
 
+let placeFormElement = document.querySelector('.popup__container_place');
+
+function placeFormSubmitHandler(evt) {
+  evt.preventDefault();
+  let nameInput = document.querySelector('.popup__input_type_title');
+  let imageInput = document.querySelector('.popup__input_type_link');
+  let cardName = nameInput.value;
+  let cardImage = imageInput.value;
+  addCard(cardName, cardImage);
+  nameInput.value = '';
+  imageInput.value = '';
+  popupPlace.classList.remove('popup_opened');
+}
+
+placeFormElement.addEventListener('submit', placeFormSubmitHandler);
 
 //селектор для контейнера с карточками
 const cardContainer = document.querySelector('.elements');
@@ -68,12 +84,18 @@ function addCard(name, image) {
   cardElement.querySelector('.element__image').src = image;
   cardElement.querySelector('.element__title').textContent = name;
 
-  cardContainer.append(cardElement);
+  cardContainer.prepend(cardElement);
+
+  const likeButton = document.querySelector('.element__like');
+  likeButton.addEventListener('click', function (evt) {
+    const eventTarget = evt.target;
+    eventTarget.classList.toggle('element__like_pressed');
+  });
 }
 
+
 //массив для карточек "из коробки"
-const initialCards = [
-  {
+const initialCards = [{
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
@@ -104,3 +126,5 @@ initialCards.forEach(function (item) {
   addCard(item.name, item.link)
 })
 
+
+//лайки
